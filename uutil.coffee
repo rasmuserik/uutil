0ce5b7e77996055b3437c898d190eac0d46164a1
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-03-18 11:35:48 +0100

    merge various utility code into the library

--- uutil.coffee
+++ uutil.coffee
@@ -11,45 +11,106 @@ use = if isNodeJs then ((module) -> require module) else ((module) -> window[mod
 # define module
 uu = if isNodeJs then exports else {}
 window.uu = uu if isWindow
-#{{{1 General utility functions
+#{{{1 Shim
+Object.keys ?= (obj) -> (key for key, _ of obj)
+#{{{1 DOM
+uu.domListen = (elem, event, fn) -> #{{{3
+  if elem.addEventListener
+    elem.addEventListener event, fn, false
+  else
+    elem.attachEvent "on#{event}", fn
+uu.onComplete = (fn) -> #{{{2
+  if isWindow
+    if document.readystate == "complete"
+      fn()
+    else
+      setTimeout (-> uu.onComplete fn), 17
 
-#{{{2 nextTick
-uu.nextTick = if isNodeJs then process.nextTick else (fn) -> setTimeout fn, 0
-#{{{2 ajax
-uu.ajax = undefined
-ajaxLegacy = false
-if isWindow then do ->
-  XHR = XMLHttpRequest
-  if typeof (new XHR).withCredentials != "boolean"
-    ajaxLegacy = true
-    XHR = XDomainRequest
-
-  uu.ajax = (url, data, cb) ->
-    xhr = new XHR()
-    xhr.onerror = (err) -> cb? err || true
-    xhr.onload = -> cb? null, xhr.responseText
-    xhr.open (if data then "POST" else "GET"), url, !!cb
-    xhr.send data
-    return xhr.responseText if !cb
-
-  if runTest then uu.nextTick ->
-    uu.ajax "//cors-test.appspot.com/test", undefined, (err, result) -> expect result, '{"status":"ok"}', "async ajax"
-    uu.ajax "//cors-test.appspot.com/test", "foo", (err, result) -> expect result, '{"status":"ok"}', "async ajax post"
-#{{{2 sleep - nicer settimeout
-#
-# Reversing order of arguments an settin tim e in seconds, makes it both easier to write and read, ie. `uu.sleep 1, -> ...`
-uu.sleep = (t,f) -> setTimeout f, t*1000
-#{{{2 extend
-uu.extend = (target, sources...) ->
+#{{{1 Objects
+uu.extend = (target, sources...) -> #{{{2
   for source in sources
     for key, val of source
       target[key] = val
   target
-#{{{2 whenDone - combining several callbacks into a single one
-#
-# Utility function for combining several callbacks into a single one. `fn = uu.whenDone(done)` returns a function `fn` where each call `done1 = fn(); done2 = fn(); ...` returns new callback functions, such that when all of `done1 done2 ...` has been called once, then done will be called.
-#
-uu.whenDone = (done) ->
+uu.deepCopy = (obj) -> #{{{2
+    if typeof obj == "object"
+      if obj.constructor == Array
+        result = []
+        result.push uu.deepCopy(e) for e in obj
+      else
+        result = {}
+        result[key] = uu.deepCopy(val) for key, val of obj
+      return result
+    else
+      return obj
+#{{{1 Array
+uu.pick = (arr) -> arr[Math.random() * arr.length | 0]
+#{{{1 Numbers
+uu.prng = (n) -> (1664525 * n + 1013904223) |0 #{{{2
+#{{{1 String
+uu.strHash = (s) -> #{{{2 hashing based on djb
+  hash = 5381
+  i = s.length
+  while i
+    hash = (hash*31 + s.charCodeAt(--i)) | 0
+  hash
+#{{{1 jsonml2html
+uu.xmlEscape = (str) -> String(str).replace RegExp("[\x00-\x1f\x80-\uffff&<>\"']", "g"), (c) -> "&##{c.charCodeAt 0};" #{{{2
+uu.obj2style = (obj) -> #{{{2
+  (for key, val of obj
+    key = key.replace /[A-Z]/g, (c) -> "-" + c.toLowerCase()
+    val = "#{val}px" if typeof val == "number"
+    "#{key}:#{val}"
+  ).join ";"
+
+uu.jsonml2html = (arr) -> #{{{2
+  return "#{uu.xmlEscape arr}" if !Array.isArray(arr)
+  # raw html, useful for stuff which shouldn't be xmlescaped etc.
+  return arr[1] if arr[0] == "rawhtml"
+  # normalise jsonml, make sure it contains attributes
+  arr = [arr[0], {}].concat arr.slice(1) if arr[1]?.constructor != Object
+  attr = {}
+  attr[key] = val for key, val of arr[1]
+  # convert style objects to strings
+  attr.style = uu.obj2style attr.style if attr.style?.constructor == Object
+  # shorthand for classes and ids
+  tag = arr[0].replace /#([^.#]*)/, ((_, id) -> attr.id = id; "")
+  tag = tag.replace /\.([^.#]*)/g, (_, cls) ->
+    attr["class"] = if attr["class"] == undefined then cls else "#{attr["class"]} #{cls}"
+    ""
+  # create actual tag string
+  result = "<#{tag}#{(" #{key}=\"#{uu.xmlEscape val}\"" for key, val of attr).join ""}>"
+  # add children and endtag, if there are children. `<foo></foo>` is done with `["foo", ""]`
+  result += "#{arr.slice(2).map(uu.jsonml2html).join ""}</#{tag}>" if arr.length > 2
+  return result
+
+#{{{2 Test / examples
+if false and runTest then process.nextTick ->
+  assert = require "assert"
+  jsonml = ["div.main",
+      style:
+        background: "red"
+        textSize: 12
+    ["h1#theHead.foo.bar", "Blåbærgrød"],
+    ["img",
+      src: "foo"
+      alt: 'the "quoted"'],
+    ["script", ["rawhtml", "console.log(foo<bar)"]]]
+
+  assert.equal jsonml2html.toString(jsonml),
+    """<div style="background:red;text-size:12px" class="main"><h1 id="theHead" class="foo bar">Bl&#229;b&#230;rgr&#248;d</h1><img src="foo" alt="the &#34;quoted&#34;"><script>console.log(foo<bar)</script></div>"""
+
+
+
+
+#{{{1 Colors
+uu.intToColor = (i) -> "#" + ((i & 0xffffff) + 0x1000000).toString(16).slice(1)
+uu.hashColor = (str) ->  -> uu.intToColor uu.prng uu.strHash str
+uu.hashColorLight = (str) -> uu.intToColor 0xe0e0e0 | ((uu.prng uu.strHash str) >> 3)
+uu.hashColorDark = (str) -> uu.intToColor ((uu.prng uu.strHash str) >> 1) & 0x7f7f7f
+#{{{1 Invoke fn
+uu.whenDone = (done) -> #{{{2
+  # Utility function for combining several callbacks into a single one. `fn = uu.whenDone(done)` returns a function `fn` where each call `done1 = fn(); done2 = fn(); ...` returns new callback functions, such that when all of `done1 done2 ...` has been called once, then done will be called.
   count = 0
   results = []
   ->
@@ -59,8 +120,7 @@ uu.whenDone = (done) ->
       args.push idx
       results.push args
       done? results if results.length == count
-#{{{2 throttleAsyncFn - throttle asynchronous function
-uu.throttleAsyncFn = (fn, delay) ->
+uu.throttleAsyncFn = (fn, delay) -> #{{{2
   delay ||= 1000
   running = []
   rerun = []
@@ -84,3 +144,82 @@ uu.throttleAsyncFn = (fn, delay) ->
   (cb) ->
     rerun.push cb
     schedule()
+uu.nextTick = if isNodeJs then process.nextTick else (fn) -> setTimeout fn, 0 #{{{2
+uu.sleep = (t,f) -> setTimeout f, t*1000 #{{{2
+# Reversing order of arguments an settin tim e in seconds, makes it both easier to write and read, ie. `uu.sleep 1, -> ...`
+#{{{1 Network
+ajaxLegacy = false
+uu.ajax = undefined #{{{2
+if isWindow then do ->
+  XHR = XMLHttpRequest
+  if typeof (new XHR).withCredentials != "boolean"
+    ajaxLegacy = true
+    XHR = XDomainRequest
+
+  uu.ajax = (url, data, cb) ->
+    xhr = new XHR()
+    xhr.onerror = (err) -> cb? err || true
+    xhr.onload = -> cb? null, xhr.responseText
+    xhr.open (if data then "POST" else "GET"), url, !!cb
+    xhr.send data
+    return xhr.responseText if !cb
+
+  if runTest then uu.nextTick ->
+    uu.ajax "//cors-test.appspot.com/test", undefined, (err, result) -> expect result, '{"status":"ok"}', "async ajax"
+    uu.ajax "//cors-test.appspot.com/test", "foo", (err, result) -> expect result, '{"status":"ok"}', "async ajax post"
+#{{{1 Logging
+#{{{2 doc+initialisation
+#
+# We want to send logging and statistics to server, 
+# but not drain battery nor exhaust the network,
+# so the log is saved to memory, and then only send across the network 
+# when more than `logBeforeSync` entries has been collected, 
+# or the user leaves the page. It is also throttled, 
+# so logging data are sent no more than once every `syncDelay` milliseconds.
+#
+# On legacy browsers we cannot send the log when the user leave the page,
+# so there we just send update every `syncDelay` milliseconds.
+#
+do ->
+  logData = []
+  logId = Math.random()
+  logUrl = "//ssl.solsort.com/api/log"
+  logSyncing = false
+  logsBeforeSync = 200
+  syncDelay = 400
+  uu.syncLog = -> #{{{2
+    if !logSyncing
+      try
+        logContent = JSON.stringify logData
+      catch e
+        logContent = "Error stringifying log"
+      logSyncing = logData
+      logData = []
+      uu.ajax logUrl, logContent, (err, result) ->
+        setTimeout (-> logSyncing = false), syncDelay
+        if err
+          log "logsync error", err
+          logData = logSyncing.concat(logData)
+        else
+          logData.push [+(new Date()), "log sync'ed", logId, logData.length]
+          uu.syncLog() if (ajaxLegacy || runTest) && logData.length > 1
+
+  uu.log = (args...) -> #{{{2
+    logData.push [+(new Date()), args...]
+    uu.nextTick uu.syncLog if logData.length > logsBeforeSync || ajaxLegacy || runTest
+    return args
+
+  uu.onComplete -> #{{{2
+    uu.domListen window, "error", (err) ->
+      uu.log "window.onerror ", String(err)
+    uu.domListen window, "beforeunload", ->
+      uu.log "window.beforeunload"
+      try
+        uu.ajax logUrl, JSON.stringify logData # blocking POST request
+      catch e
+        undefined
+      undefined
+    uu.log "starting", logId, window.performance
+    uu.log "userAgent", navigator.userAgent
+
+
--- uutil.js
+++ uutil.js
@@ -34,52 +34,36 @@
     window.uu = uu;
   }
 
-  uu.nextTick = isNodeJs ? process.nextTick : function(fn) {
-    return setTimeout(fn, 0);
-  };
-
-  uu.ajax = void 0;
-
-  ajaxLegacy = false;
-
-  if (isWindow) {
-    (function() {
-      var XHR;
-      XHR = XMLHttpRequest;
-      if (typeof (new XHR).withCredentials !== "boolean") {
-        ajaxLegacy = true;
-        XHR = XDomainRequest;
+  if (Object.keys == null) {
+    Object.keys = function(obj) {
+      var key, _, _results;
+      _results = [];
+      for (key in obj) {
+        _ = obj[key];
+        _results.push(key);
       }
-      uu.ajax = function(url, data, cb) {
-        var xhr;
-        xhr = new XHR();
-        xhr.onerror = function(err) {
-          return typeof cb === "function" ? cb(err || true) : void 0;
-        };
-        xhr.onload = function() {
-          return typeof cb === "function" ? cb(null, xhr.responseText) : void 0;
-        };
-        xhr.open((data ? "POST" : "GET"), url, !!cb);
-        xhr.send(data);
-        if (!cb) {
-          return xhr.responseText;
-        }
-      };
-      if (runTest) {
-        return uu.nextTick(function() {
-          uu.ajax("//cors-test.appspot.com/test", void 0, function(err, result) {
-            return expect(result, '{"status":"ok"}', "async ajax");
-          });
-          return uu.ajax("//cors-test.appspot.com/test", "foo", function(err, result) {
-            return expect(result, '{"status":"ok"}', "async ajax post");
-          });
-        });
-      }
-    })();
+      return _results;
+    };
   }
 
-  uu.sleep = function(t, f) {
-    return setTimeout(f, t * 1000);
+  uu.domListen = function(elem, event, fn) {
+    if (elem.addEventListener) {
+      return elem.addEventListener(event, fn, false);
+    } else {
+      return elem.attachEvent("on" + event, fn);
+    }
+  };
+
+  uu.onComplete = function(fn) {
+    if (isWindow) {
+      if (document.readystate === "complete") {
+        return fn();
+      } else {
+        return setTimeout((function() {
+          return uu.onComplete(fn);
+        }), 17);
+      }
+    }
   };
 
   uu.extend = function() {
@@ -93,6 +77,153 @@
       }
     }
     return target;
+  };
+
+  uu.deepCopy = function(obj) {
+    var e, key, result, val, _i, _len;
+    if (typeof obj === "object") {
+      if (obj.constructor === Array) {
+        result = [];
+        for (_i = 0, _len = obj.length; _i < _len; _i++) {
+          e = obj[_i];
+          result.push(uu.deepCopy(e));
+        }
+      } else {
+        result = {};
+        for (key in obj) {
+          val = obj[key];
+          result[key] = uu.deepCopy(val);
+        }
+      }
+      return result;
+    } else {
+      return obj;
+    }
+  };
+
+  uu.pick = function(arr) {
+    return arr[Math.random() * arr.length | 0];
+  };
+
+  uu.prng = function(n) {
+    return (1664525 * n + 1013904223) | 0;
+  };
+
+  uu.strHash = function(s) {
+    var hash, i;
+    hash = 5381;
+    i = s.length;
+    while (i) {
+      hash = (hash * 31 + s.charCodeAt(--i)) | 0;
+    }
+    return hash;
+  };
+
+  uu.xmlEscape = function(str) {
+    return String(str).replace(RegExp("[\x00-\x1f\x80-\uffff&<>\"']", "g"), function(c) {
+      return "&#" + (c.charCodeAt(0)) + ";";
+    });
+  };
+
+  uu.obj2style = function(obj) {
+    var key, val;
+    return ((function() {
+      var _results;
+      _results = [];
+      for (key in obj) {
+        val = obj[key];
+        key = key.replace(/[A-Z]/g, function(c) {
+          return "-" + c.toLowerCase();
+        });
+        if (typeof val === "number") {
+          val = "" + val + "px";
+        }
+        _results.push("" + key + ":" + val);
+      }
+      return _results;
+    })()).join(";");
+  };
+
+  uu.jsonml2html = function(arr) {
+    var attr, key, result, tag, val, _ref, _ref1, _ref2;
+    if (!Array.isArray(arr)) {
+      return "" + (uu.xmlEscape(arr));
+    }
+    if (arr[0] === "rawhtml") {
+      return arr[1];
+    }
+    if (((_ref = arr[1]) != null ? _ref.constructor : void 0) !== Object) {
+      arr = [arr[0], {}].concat(arr.slice(1));
+    }
+    attr = {};
+    _ref1 = arr[1];
+    for (key in _ref1) {
+      val = _ref1[key];
+      attr[key] = val;
+    }
+    if (((_ref2 = attr.style) != null ? _ref2.constructor : void 0) === Object) {
+      attr.style = uu.obj2style(attr.style);
+    }
+    tag = arr[0].replace(/#([^.#]*)/, (function(_, id) {
+      attr.id = id;
+      return "";
+    }));
+    tag = tag.replace(/\.([^.#]*)/g, function(_, cls) {
+      attr["class"] = attr["class"] === void 0 ? cls : "" + attr["class"] + " " + cls;
+      return "";
+    });
+    result = "<" + tag + (((function() {
+      var _results;
+      _results = [];
+      for (key in attr) {
+        val = attr[key];
+        _results.push(" " + key + "=\"" + (uu.xmlEscape(val)) + "\"");
+      }
+      return _results;
+    })()).join("")) + ">";
+    if (arr.length > 2) {
+      result += "" + (arr.slice(2).map(uu.jsonml2html).join("")) + "</" + tag + ">";
+    }
+    return result;
+  };
+
+  if (false && runTest) {
+    process.nextTick(function() {
+      var assert, jsonml;
+      assert = require("assert");
+      jsonml = [
+        "div.main", {
+          style: {
+            background: "red",
+            textSize: 12
+          }
+        }, ["h1#theHead.foo.bar", "Blåbærgrød"], [
+          "img", {
+            src: "foo",
+            alt: 'the "quoted"'
+          }
+        ], ["script", ["rawhtml", "console.log(foo<bar)"]]
+      ];
+      return assert.equal(jsonml2html.toString(jsonml), "<div style=\"background:red;text-size:12px\" class=\"main\"><h1 id=\"theHead\" class=\"foo bar\">Bl&#229;b&#230;rgr&#248;d</h1><img src=\"foo\" alt=\"the &#34;quoted&#34;\"><script>console.log(foo<bar)</script></div>");
+    });
+  }
+
+  uu.intToColor = function(i) {
+    return "#" + ((i & 0xffffff) + 0x1000000).toString(16).slice(1);
+  };
+
+  uu.hashColor = function(str) {
+    return function() {
+      return uu.intToColor(uu.prng(uu.strHash(str)));
+    };
+  };
+
+  uu.hashColorLight = function(str) {
+    return uu.intToColor(0xe0e0e0 | ((uu.prng(uu.strHash(str))) >> 3));
+  };
+
+  uu.hashColorDark = function(str) {
+    return uu.intToColor(((uu.prng(uu.strHash(str))) >> 1) & 0x7f7f7f);
   };
 
   uu.whenDone = function(done) {
@@ -151,5 +282,117 @@
       return schedule();
     };
   };
+
+  uu.nextTick = isNodeJs ? process.nextTick : function(fn) {
+    return setTimeout(fn, 0);
+  };
+
+  uu.sleep = function(t, f) {
+    return setTimeout(f, t * 1000);
+  };
+
+  ajaxLegacy = false;
+
+  uu.ajax = void 0;
+
+  if (isWindow) {
+    (function() {
+      var XHR;
+      XHR = XMLHttpRequest;
+      if (typeof (new XHR).withCredentials !== "boolean") {
+        ajaxLegacy = true;
+        XHR = XDomainRequest;
+      }
+      uu.ajax = function(url, data, cb) {
+        var xhr;
+        xhr = new XHR();
+        xhr.onerror = function(err) {
+          return typeof cb === "function" ? cb(err || true) : void 0;
+        };
+        xhr.onload = function() {
+          return typeof cb === "function" ? cb(null, xhr.responseText) : void 0;
+        };
+        xhr.open((data ? "POST" : "GET"), url, !!cb);
+        xhr.send(data);
+        if (!cb) {
+          return xhr.responseText;
+        }
+      };
+      if (runTest) {
+        return uu.nextTick(function() {
+          uu.ajax("//cors-test.appspot.com/test", void 0, function(err, result) {
+            return expect(result, '{"status":"ok"}', "async ajax");
+          });
+          return uu.ajax("//cors-test.appspot.com/test", "foo", function(err, result) {
+            return expect(result, '{"status":"ok"}', "async ajax post");
+          });
+        });
+      }
+    })();
+  }
+
+  (function() {
+    var logData, logId, logSyncing, logUrl, logsBeforeSync, syncDelay;
+    logData = [];
+    logId = Math.random();
+    logUrl = "//ssl.solsort.com/api/log";
+    logSyncing = false;
+    logsBeforeSync = 200;
+    syncDelay = 400;
+    uu.syncLog = function() {
+      var e, logContent;
+      if (!logSyncing) {
+        try {
+          logContent = JSON.stringify(logData);
+        } catch (_error) {
+          e = _error;
+          logContent = "Error stringifying log";
+        }
+        logSyncing = logData;
+        logData = [];
+        return uu.ajax(logUrl, logContent, function(err, result) {
+          setTimeout((function() {
+            return logSyncing = false;
+          }), syncDelay);
+          if (err) {
+            log("logsync error", err);
+            return logData = logSyncing.concat(logData);
+          } else {
+            logData.push([+(new Date()), "log sync'ed", logId, logData.length]);
+            if ((ajaxLegacy || runTest) && logData.length > 1) {
+              return uu.syncLog();
+            }
+          }
+        });
+      }
+    };
+    uu.log = function() {
+      var args;
+      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      logData.push([+(new Date())].concat(__slice.call(args)));
+      if (logData.length > logsBeforeSync || ajaxLegacy || runTest) {
+        uu.nextTick(uu.syncLog);
+      }
+      return args;
+    };
+    return uu.onComplete(function() {
+      uu.domListen(window, "error", function(err) {
+        return uu.log("window.onerror ", String(err));
+      });
+      uu.domListen(window, "beforeunload", function() {
+        var e;
+        uu.log("window.beforeunload");
+        try {
+          uu.ajax(logUrl, JSON.stringify(logData));
+        } catch (_error) {
+          e = _error;
+          void 0;
+        }
+        return void 0;
+      });
+      uu.log("starting", logId, window.performance);
+      return uu.log("userAgent", navigator.userAgent);
+    });
+  })();
 
 }).call(this);
